2c90f91bb8f57e21b0d3421ef09177a72fb3c637
Michael <example@gmail.com>
2020-10-17 16:24:15 +0800

    forms almost done, need phoneInput mask

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -2,6 +2,7 @@ import postData from "../services/postData";
 
 const form = (formSelector) => {
     const forms = document.querySelectorAll(formSelector);
+    const upload = document.querySelectorAll('[name = "upload"]');
     const messageBox = {
         loading: "идет отправка",
         success: "Спасибо! Скоро мы с вами свяжемся",
@@ -10,22 +11,23 @@ const form = (formSelector) => {
         ok: "assets/img/ok.png",
         fail: "assets/img/fail.png"
     };
+    const path = {
+        designer: "assets/server.php",
+        question: "assets/question.php"
+    };
     let statusMessage;
-    let fileUploadInputs = document.querySelectorAll('[type = "file"]');
-    fileUploadInputs.forEach(input => {
+    
+    upload.forEach(input => {
         input.addEventListener('input', () => {
-            
-            if(input.files[0].name.length > 6) {
-                console.log('here');
-                input.files[0].name.split(0, 6);
-            }else {
-                input.files[0].name.split(0, 9);
-                console.log('here2');
-            }
-            console.log(input.files[0].name);
+            let dots;
+            let arr = input.files[0].name.split('.');
+            arr[0].length > 6 ? dots = '...' : dots ='.';
+            const name = arr[0] + dots + arr[1];   
+            input.previousElementSibling.textContent = name;
         });
         
     });
+    
     forms.forEach(form => {
         let phoneInputs = document.querySelectorAll('[name = phone]');
         // phoneInputs.forEach(input => {
@@ -38,9 +40,7 @@ const form = (formSelector) => {
         
         form.addEventListener('submit', (e) => {
             e.preventDefault();
-            
-            
-
+   
             statusMessage = document.createElement('div');
             let statusImg = document.createElement('img');
             statusImg.style.cssText = `
@@ -48,6 +48,7 @@ const form = (formSelector) => {
                 margin: 0 auto;
             `;
             statusImg.setAttribute('src', messageBox.spinner);
+            statusImg.classList.add('animated', 'fadeInUp');
             statusMessage.style.cssText = `
                 margin: 30px auto 0 auto;
                 font-size: 20px;
@@ -55,34 +56,42 @@ const form = (formSelector) => {
                 text-align: center;
             `;
             statusMessage.textContent = messageBox.loading;
-            form.append(statusImg);
-            form.append(statusMessage); 
+            form.parentNode.append(statusImg);
+            form.parentNode.append(statusMessage); 
+            form.classList.add('animated', 'fadeOutUp');
+            setTimeout(() => {
+                form.style.display = 'none';
+            }, 400);
             
 
             let formData = new FormData(form);
+            let api;
             
-            postData("../../../assets/server.php", formData)
+            form.closest('.popup-design') || form.classList.contains('calc_form') ? api = path.designer : api = path.question;
+            console.log(api);
+
+
+            postData(api, formData)
                 .then(data => console.log(data))
                 .then(() => {
-                    form.style.display = 'none';
                     statusMessage.textContent = messageBox.success;
                     statusImg.setAttribute('src', messageBox.ok);
-                    form.parentNode.append(statusImg);
-                    form.parentNode.append(statusMessage);
                     
                 })
                 .catch(() => {
-                    form.style.display = 'none';
                     statusMessage.textContent = messageBox.failure;
                     statusImg.setAttribute('src', messageBox.fail);
-                    form.parentNode.append(statusImg, statusMessage);
                 })
                 .finally(() => {
                     form.reset();
+                    upload.forEach(input => {
+                        input.previousElementSibling.textContent = 'Файл не выбран';
+                    });
                     setTimeout(() => {
                         statusMessage.remove();
                         statusImg.remove();
-                        form.classList.add('animated', 'fadeInDown');
+                        form.classList.remove('fadeOutUp');
+                        form.classList.add('fadeInUp');
                         form.style.display = 'block';
                     }, 3000);
                     
